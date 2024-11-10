48344891c41922fd1e167d9747eb50fa786e6cf3
Frootyloops44 <frootyloops44@gmail.com>
2024-11-09 22:58:28 -0600

    config fix

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -6,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC4w7EXmG7BJapsfKGzOyCIl7EhKZY5sE4",
   authDomain: "psifl-ba8fa.firebaseapp.com",
   projectId: "psifl-ba8fa",
-  storageBucket: "psifl-ba8fa.firebasestorage.app",
+  storageBucket: "psifl-ba8fa.appspot.com", // Fixed this line
   messagingSenderId: "727467505131",
   appId: "1:727467505131:web:a224db716a889f433455ae",
 };
